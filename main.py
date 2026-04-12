eaf1db27f199089bf34f1e855f2dfdbf24a42a75
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2026-04-12 23:06:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from models import Product
+from database_models import Base
+from dbconnection import engine
+from dbconnection import session
+from database_models import ProductDB
+from sqlalchemy.orm import Session 
 app=FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def greet():
-    return "Hello, to my website"
+    return {"message":"Hello, welcome to the fastapi guys"}
 
 # list to send dummy data
 
@@ -15,47 +23,81 @@ products=[
     Product(id=5,name="Laptop",brand="asus",description="A high quality laptop",price=45.9,quantity=10)
 ]
 
- 
+def _get_db():
+    db=session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def __init__db():
+    db=session()
+    count=db.query(ProductDB).count()
+    print(count)
+    if count==0:
+        for product in products:
+            db.add(ProductDB(**product.model_dump()))
+        db.commit()
+__init__db()
 @app.get("/products")
-def get_all_products():
-    return products
+def get_all_products(db:Session=Depends(_get_db)):
+    prod_db=db.query(ProductDB).all()
+    return prod_db
 
 @app.get("/products/{id}")
 
-def get_product_by_id(id:int):
-    for i in range(len(products)):
-        if(products[i].id==id):
-            return products[i]
+def get_product_by_id(id:int,db:Session=Depends(_get_db)):
+    # for i in range(len(products)):
+    #     if(products[i].id==id):
+    #         return products[i]
+    prod_db=db.query(ProductDB).filter(ProductDB.id==id).first()    
+    if prod_db:
+        return prod_db
+
         
-    return "product with id not present "
+    return {"message":"product with id not found"}
         
 
 @app.post("/products")
-def add_product_details(product:Product):
+def add_product_details(product:Product,db:Session=Depends(_get_db)):
+    new_product=ProductDB(**product.model_dump())
+    db.add(new_product)
+    db.commit()
 
-    products.append(product)
+    
     return "product added successfully"
 
-@app.put("/product/{id}")
-def update_individual_product(id:int,updatedproduct:Product):
-    for product in products:
-        if product.id==id:
-            product.name=updatedproduct.name
-            product.brand=updatedproduct.brand
-            product.description=updatedproduct.description
-            product.price=updatedproduct.price
-            product.quantity=updatedproduct.quantity
+@app.put("/products/{id}")
+def update_individual_product(id:int,updatedproduct:Product,db:Session=Depends(_get_db)):
+    # for product in products:
+    #     if product.id==id:
+    #         product.name=updatedproduct.name
+    #         product.brand=updatedproduct.brand
+    #         product.description=updatedproduct.description
+    #         product.price=updatedproduct.price
+    #         product.quantity=updatedproduct.quantity
 
-            return "product updated successfully"   
-    return "product with the id does not exits"
+    #         return "product updated successfully"   
+    prod_db=db.query(ProductDB).filter(ProductDB.id==id).first() 
+    if prod_db:
+        prod_db.brand=updatedproduct.brand
+        prod_db.name=updatedproduct.name
+        prod_db.description=updatedproduct.description
+        prod_db.price=updatedproduct.price
+        prod_db.quantity=updatedproduct.quantity
+        db.commit()
+        return {"message":"product updated successfully."}
+    return {
+        "message":"product with id not found"
+    }
             
 
-@app.delete("/product/{id}")
-def delete_product_by_id(id:int):
-    for  prod in products:
-        if prod.id==id:
-            products.remove(prod)
-            return {
-                "message":"Product removed successfully"
-                
-            }
+@app.delete("/products/{id}")
+def delete_product_by_id(id:int,db:Session=Depends(_get_db)):
+    prod_db=db.query(ProductDB).filter(ProductDB.id==id).first()
+    if prod_db:
+        db.delete(prod_db)
+        db.commit()
+        return {"message":"product deleted successfully"}
+    return {"message":"product not found"}
+  
